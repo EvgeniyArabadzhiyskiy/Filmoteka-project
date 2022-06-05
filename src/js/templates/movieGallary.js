528288730ad9d38db5987e76b1331a06f6f85ce3
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-06-05 19:39:50 +0300

    Update movieGallary.js

--- src/js/templates/movieGallary.js
+++ src/js/templates/movieGallary.js
@@ -1,5 +1,7 @@
 
 import BASE_URL from "../apiService";
+//import getAllGenres from "../getGenres";
+const IMG_URL= `https://image.tmdb.org/t/p/w500`;
 const mainContainer = document.querySelector('.movie-card-list');
   
 
@@ -10,7 +12,7 @@ export default function renderGallery(results) {
     const markup = results.map(({ poster_path, original_title, release_date, genre_ids}) => {
       return `<li id="" class="movie-card-item">
 				<div class="movie-poster__container">
-					<img src="${BASE_URL}${poster_path}" alt="poster" />
+					<img src="${IMG_URL}${poster_path}" alt="poster" />
 				</div>
 				<div class="movie-card-item__info">
 					<h2 class="movie-card-item__title">${original_title}</h2>
@@ -18,6 +20,7 @@ export default function renderGallery(results) {
 			</li>`;
     }).join(''); 
   mainContainer.insertAdjacentHTML("beforeend", markup);
+  
   }
 
 
