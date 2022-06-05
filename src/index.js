46a0d6be366df2a55bfadc7a2b79d9c5b066823f
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-06-05 16:43:00 +0300

    add render page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import apiService from "./js/apiService";
+import renderGallery from "./js/templates/movieGallary";
 
 
-
-apiService.fetchData;
+//apiService.fetchData;
+const mainContainer = document.querySelector('.movie-card__container');
+apiService.fetchTrendData().then((data)=> {
+  console.log(data.results);
+  renderGallery(data.results);
+}).catch(error => console.log(error));
+apiService.fetchGenresList().then((data)=> {
+  console.log(data);
+})
