95f8d59e9c7953491a16d109deaaaa6196b08157
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-06-06 00:57:02 +0300

    fixsed/genres

--- src/js/templates/movieGallary.js
+++ src/js/templates/movieGallary.js
@@ -4,23 +4,23 @@ import BASE_URL from "../apiService";
 const IMG_URL= `https://image.tmdb.org/t/p/w500`;
 const mainContainer = document.querySelector('.movie-card-list');
  
-export default function renderGallery(results, {genres}) {
+export default function renderGallery(results, ) {
     const markup = results.map(({ poster_path, original_title, release_date, genre_ids}) => {
-	const genresNames = genre_ids.reduce((acc, itemId, index) => {
-		if(!genre_ids.length){
-			return acc;
-		}
-		else if(index <= 1){
-			const genreName = genres.find(({id})  => id=== itemId)
-			if(index <= genre_ids.length -2){
-				return acc + genreName.name + ', ';
-			}
-				return acc + genreName.name + ' ';
-			} else if( index === 2){
-				return acc + "Others";
-			} 
-		return acc;
-	},"")	
+	// const genresNames = genre_ids.reduce((acc, itemId, index) => {
+	// 	if(!genre_ids.length){
+	// 		return acc;
+	// 	}
+	// 	else if(index <= 1){
+	// 		const genreName = genres.find(({id})  => id=== itemId)
+	// 		if(index <= genre_ids.length -2){
+	// 			return acc + genreName.name + ', ';
+	// 		}
+	// 			return acc + genreName.name + ' ';
+	// 		} else if( index === 2){
+	// 			return acc + "Others";
+	// 		} 
+	// 	return acc;
+	// },"")	
 	const releaseYear = release_date.split("-").slice(0,1);
       return `<li id="" class="movie-card-item">
 				<div class="movie-poster__container">
@@ -28,7 +28,7 @@ export default function renderGallery(results, {genres}) {
 				</div>
 				<div class="movie-card-item__info">
 					<h2 class="movie-card-item__title">${original_title}</h2>
-					<p class="movie-card-item__text">${genresNames} | ${releaseYear} </p>
+					<p class="movie-card-item__text">${genre_ids} | ${releaseYear} </p>
 			</li>`;
     }).join(''); 
   mainContainer.insertAdjacentHTML("beforeend", markup);
