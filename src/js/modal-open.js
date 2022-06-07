0f0ee8aa8609ae003f67e06637eedff6fe1fe203
ZeinHome <zein3288@gmail.com>
2022-06-07 20:58:12 +0300

    close modal
    через ESC, через backdrop

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -1,6 +1,6 @@
 import apiService from './apiService'
 import renderMovieModal from './templates/renderMovieModal';
-import {addToWatched} from './addToWatched';
+import { addToWatched } from './addToWatched';
 import { addToQueue } from './addToQueue';
 import removeFromWatched from './removeFromWatched';
 import removeFromQueue from './removeFromQueue';
@@ -11,7 +11,7 @@ const modalMovieContainer = document.querySelector('.film-content');
 const modalButtonClose = document.querySelector('.modal__button-close');
 
 
-export default function openModal(){
+export default function openModal() {
 
     filmList.addEventListener('click', onClick)
 
@@ -22,9 +22,11 @@ async function onClick(e) {
     if (e.target.nodeName !== "IMG" && e.target.nodeName !== "H2") {
         return
     }
+    document.body.classList.add("modal-open");
     modal.classList.remove('is-hidden')
     modalButtonClose.addEventListener('click', modalClose)
     window.addEventListener("keyup", press)
+    window.addEventListener("click", onCloseBackdropClick)
 
     const filmId = e.target.dataset.id;
 
@@ -33,7 +35,7 @@ async function onClick(e) {
     const createMarkupFilmInModal = await renderMovieModal(fullMovieInfo);
 
     modalMovieContainer.insertAdjacentHTML("beforeend", createMarkupFilmInModal);
-    
+
     const addToWatchedBtn = document.querySelector('.btn-watched');
     const addToQueueBtn = document.querySelector('.btn-qweqwe');
 
@@ -61,8 +63,8 @@ async function onClick(e) {
 }
 
 
-function press(e){
-    if(e.code === "Escape"){
+function press(e) {
+    if (e.code === "Escape") {
         modalClose();
         window.removeEventListener("keyup", press);
         modalButtonClose.removeEventListener('click', modalClose);
@@ -70,11 +72,19 @@ function press(e){
     return
 }
 
+function onCloseBackdropClick(e) {
+    if (e.target.dataset.close === 'backdrop') {
+        modalClose();
 
-function modalClose(e){
+    }
+    return;
+}
+
+function modalClose(e) {
+    document.body.classList.remove("modal-open");
     modal.classList.add('is-hidden');
     window.removeEventListener("keyup", press);
+    window.removeEventListener("click", onCloseBackdropClick);
     modalButtonClose.removeEventListener('click', modalClose);
     modalMovieContainer.innerHTML = "";
 }
-
