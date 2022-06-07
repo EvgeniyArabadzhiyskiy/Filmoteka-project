3a9097b7c1f8ca646ee6d371b1d3520ee315a901
Preksler <preksler@gmail.com>
2022-06-07 14:50:29 +0300

    add defoult value

--- src/js/templates/movieGallary.js
+++ src/js/templates/movieGallary.js
@@ -1,12 +1,18 @@
-
-
+import noPoster from '../../images/no-poster.jpg';
 const IMG_URL= `https://image.tmdb.org/t/p/w500`;
 const mainContainer = document.querySelector('.movie-card-list');
  
-export default function renderGallery(results,{genres} ) {
-    const markup = results.map(({ poster_path, original_title, release_date, genre_ids,id}) => {
-	const genresNames = genre_ids.reduce((acc, itemId, index) => {
-		if(!genre_ids.length){
+export default function renderGallery(results, { genres }) {
+	const markup = results.map(({ poster_path, original_title, release_date, genre_ids, id }) => {
+		if (original_title === "") {
+        	original_title = "Sorry, no title for the movie.";
+		}
+		if (release_date === "") {
+			release_date = "No release date";
+		}
+		let imagePoster = poster_path ? `${IMG_URL}${poster_path}` : noPoster;		
+		const genresNames = genre_ids.reduce((acc, itemId, index) => {
+		if (!genre_ids.length) {
 			return acc;
 		}
 		else if(index <= 1){
@@ -19,15 +25,15 @@ export default function renderGallery(results,{genres} ) {
 				return acc + "Others";
 			} 
 		return acc;
-	},"")	
-	const releaseYear = release_date.split("-").slice(0,1);
+	}, "")	
+		const releaseYear = release_date.split("-").slice(0, 1);
       return `<li  class="movie-card-item">
 				<div class="movie-poster__container">
-					<img src="${IMG_URL}${poster_path}" alt="poster" data-id="${id}"/>
+					<img src="${imagePoster}" alt="poster" data-id="${id}"/>
 				</div>
 				<div class="movie-card-item__info">
 					<h2 class="movie-card-item__title" data-id="${id}">${original_title}</h2>
-					<p class="movie-card-item__text">${genresNames} | ${releaseYear} </p>
+					<p class="movie-card-item__text">${genresNames?genresNames:"No genres"} | ${releaseYear} </p>
 			</li>`;
     }).join(''); 
   mainContainer.insertAdjacentHTML("beforeend", markup);
