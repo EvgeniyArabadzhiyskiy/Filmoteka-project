a32c1eead666f0106e79672c62755705b2d1d4dc
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-06-08 20:18:12 +0300

    Update movieGallary.js

--- src/js/templates/movieGallary.js
+++ src/js/templates/movieGallary.js
@@ -2,13 +2,15 @@ import noPoster from '../../images/no-poster.jpg';
 const IMG_URL= `https://image.tmdb.org/t/p/w500`;
 const mainContainer = document.querySelector('.movie-card-list');
  
-export default function renderGallery(results, { genres }) {
+export default function renderGallery(results, dataGenres) {
+    console.log("renderGallery ~ dataGenres", dataGenres);
+	const genres = dataGenres?.genres
 	const markup = results.map(({ poster_path, original_title, release_date, genre_ids, id }) => {
 		if (original_title === "") {
         	original_title = "Sorry, no title for the movie.";
 		}
-		if (release_date === "") {
-			release_date = "No release date";
+		if (!release_date) {
+			release_date = "Expected";
 		}
 		let imagePoster = poster_path ? `${IMG_URL}${poster_path}` : noPoster;		
 		const genresNames = genre_ids.reduce((acc, itemId, index) => {
@@ -16,16 +18,17 @@ export default function renderGallery(results, { genres }) {
 			return acc;
 		}
 		else if(index <= 1){
-			const genreName = genres.find(({id})  => id=== itemId)
+			const genreName = genres?.find(({id})  => id=== itemId)
 			if(index <= genre_ids.length -2){
-				return acc + genreName.name + ', ';
+				return acc + genreName?.name + ', ';
 			}
-				return acc + genreName.name + ' ';
+				return acc + genreName?.name + ' ';
 			} else if( index === 2){
 				return acc + "Others";
 			} 
 		return acc;
 	}, "")	
+		const genresNamesUPGRADE = genres ? genresNames : "Other" 
 		const releaseYear = release_date?.split("-").slice(0, 1);
       return `<li  class="movie-card-item">
 				<div class="movie-poster__container">
@@ -33,7 +36,7 @@ export default function renderGallery(results, { genres }) {
 				</div>
 				<div class="movie-card-item__info">
 					<h2 class="movie-card-item__title" data-id="${id}">${original_title}</h2>
-					<p class="movie-card-item__text">${genresNames?genresNames:"No genres"} | ${releaseYear} </p>
+					<p class="movie-card-item__text">${genresNamesUPGRADE} | ${releaseYear} </p>
 			</li>`;
     }).join(''); 
   mainContainer.insertAdjacentHTML("beforeend", markup);
