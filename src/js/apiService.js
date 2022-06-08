91517b092076ad9ace84efc0f0792b00969d4171
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-06-08 19:12:30 +0300

    fix searh elements

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -5,7 +5,7 @@ const API_KEY = '33da1ae0687d870981cf19ad4813876b';
 const TREND_URL= `${BASE_URL}/trending/movie/day`;
 const URL_ID = `${BASE_URL}/movie/`;
 let allGenres;
-const input = document.querySelector(".form__search").elements.search;
+const input = document.querySelector(".form__search")?.elements.search;
 let movieInfo;
 
 export default {
