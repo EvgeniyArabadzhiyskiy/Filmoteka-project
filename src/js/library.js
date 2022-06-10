df12f1a747e0ff52570cfbde15b456092c3f2d7a
Preksler <preksler@gmail.com>
2022-06-10 14:13:23 +0300

    add modal

--- src/js/library.js
+++ src/js/library.js
@@ -1,5 +1,5 @@
 import  "./templates/darkThema";
-import openModal from './modalLibrary';
+import openModal from "./modal-open";
 import noPoster from '../images/no-poster.jpg';
 
 const IMG_URL = `https://image.tmdb.org/t/p/w500`;
@@ -36,7 +36,7 @@ function onWatchedMoviesClick() {
     markup(data);
 }
 
-function onQueueMoviesClick(e) {
+function onQueueMoviesClick() {
     refs.queueMovies.classList.add('library-btn__isActive');
     refs.watchedMovies.classList.remove('library-btn__isActive');
     localStorage.setItem('isActive', 'queue');
@@ -53,15 +53,36 @@ function onQueueMoviesClick(e) {
 
 function markup(data) {
     const markup = data.map(({ poster_path, original_title, vote_average, genres, release_date, id }) => {
+        if (original_title === "") {
+        	original_title = "Sorry, no title for the movie.";
+		}
+		if (!release_date) {
+			release_date = "Expected";
+		}
         let imagePoster = poster_path ? `${IMG_URL}${poster_path}` : noPoster;
         const releaseYear = release_date.split("-").slice(0, 1);
+        const genresNames = genres.reduce((acc, { name }, index) => {
+        if (!genres.length) {
+          return acc;
+        }
+        else if (index <= 1) {
+          if (index <= genres.length - 2) {
+            return acc + name + ', ';
+          }
+          return acc + name + ' ';
+        }
+        else if (index === 2) {
+          return acc + 'Others';
+        }
+        return acc;
+      },'');	
         return `<li  class="movie-card-item">
 				<div class="movie-poster__container">
 					<img src="${imagePoster}" alt="poster" class="imagePoster" data-id="${id}"/>
 				</div>
 				<div class="movie-card-item__info">
 					<h2 class="movie-card-item__title" data-id="${id}">${original_title}</h2>
-					<p class="movie-card-item__text">${genres.map(genre => genre.name).join(', ')} | ${releaseYear} <span class="rating">${vote_average}</span></p>
+					<p class="movie-card-item__text">${genresNames?genresNames:"No genres"} | ${releaseYear} <span class="rating">${vote_average}</span></p>
 			</li>`;
     })
     refs.moviesList.innerHTML = markup.join('');
