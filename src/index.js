d5307a4310dd95d49d8b8fcb5b66d17d7c3a733c
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-06-12 19:42:55 +0300

    refact for movieGallary

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+// import "./js/darkThema";
+// import "./js/cursor";
+// import apiService from "./js/apiService";
+// import renderGallery from "./js/templates/movieGallary";
+// import { renderPaginationButtons } from "./js/pagination";
+// import renderMovieModal from "./js/templates/renderMovieModal";
+// import openModal from "./js/modal-open";
+// import onSearchButton from "./js/movieSearch";
+// import NProgress from 'nprogress';
+
+// import "./js/team-modal";
+// import "./js/sliderTrendingMovies";
+// import { renderMovieCardsSlider } from "./js/templates/renderMovieCardsSlider";
+// import './js/helpers/back-to-top';
+
+// let allGenres;
+// const filmList = document.querySelector('.movie-card-list'); 
+
+// NProgress.start();
+// openModal(filmList);
+
+// const mainContainer = document.querySelector('.movie-card__container');
+// apiService.fetchGenresList().then((data) => {
+//   allGenres = data;
+// })
+
+// apiService.fetchTrendData().then((data) => {
+ 
+//   renderMovieCardsSlider(data.results);
+
+//   renderGallery(data.results, allGenres);
+
+//   renderPaginationButtons(data.total_pages, data.page);
+
+// }).catch(error => console.log(error));
+// NProgress.done();
+
+
+
+
+// =======================================================================================
+
 import "./js/darkThema";
 import "./js/cursor";
 import apiService from "./js/apiService";
@@ -12,70 +54,30 @@ import "./js/team-modal";
 import "./js/sliderTrendingMovies";
 import { renderMovieCardsSlider } from "./js/templates/renderMovieCardsSlider";
 import './js/helpers/back-to-top';
+import {markupGallery} from './js/templates/startPageMarkup'
 
 let allGenres;
+const addToWatchedBtn = document.querySelector('.btn-watched');
 const filmList = document.querySelector('.movie-card-list'); 
+const mainContainer = document.querySelector('.movie-card-list');
+
 
 NProgress.start();
-openModal(filmList);
+openModal(filmList)
 
-const mainContainer = document.querySelector('.movie-card__container');
-apiService.fetchGenresList().then((data) => {
+
+apiService.fetchGenresList().then((data)=> {
   allGenres = data;
 })
 
-apiService.fetchTrendData().then((data) => {
- 
-  renderMovieCardsSlider(data.results);
-
-  renderGallery(data.results, allGenres);
-
-  renderPaginationButtons(data.total_pages, data.page);
-
-}).catch(error => console.log(error));
-NProgress.done();
-
-
-
-
-// =======================================================================================
-
-// import  "./js/templates/darkThema";
-// import apiService from "./js/apiService";
-// import renderGallery from "./js/templates/movieGallary";
-// import { renderPaginationButtons } from "./js/pagination";
-// import renderMovieModal from "./js/templates/renderMovieModal";
-// import openModal from "./js/modal-open";
-// import onSearchButton from "./js/movieSearch";
-// import NProgress from 'nprogress';
-// import "./js/team-modal";
-// import "./js/sliderTrendingMovies";
-// import { renderMovieCardsSlider } from "./js/templates/renderMovieCardsSlider";
-
-// import {markupGallery} from './js/templates/movieGallary'
-
-// let allGenres;
-// const addToWatchedBtn = document.querySelector('.btn-watched');
-// const filmList = document.querySelector('.movie-card-list'); 
-// const mainContainer = document.querySelector('.movie-card-list');
-
-
-// NProgress.start();
-// openModal(filmList)
-
-
-// apiService.fetchGenresList().then((data)=> {
-//   allGenres = data;
-// })
-
-// apiService.fetchTrendData().then((data)=> {
+apiService.fetchTrendData().then((data)=> {
   
 
-//   const filmData = renderGallery(data.results, allGenres);
-//   const markupMovie = markupGallery(filmData)
-//   mainContainer.insertAdjacentHTML("beforeend", markupMovie);
+  const filmData = renderGallery(data.results, allGenres);
+  const markupMovie = markupGallery(filmData)
+  mainContainer.insertAdjacentHTML("beforeend", markupMovie);
+  renderMovieCardsSlider(data.results);
+  renderPaginationButtons(data.total_pages, data.page);
 
-//   renderPaginationButtons(data.total_pages, data.page);
-
-// }).catch(error => console.log(error))
-// NProgress.done()
+}).catch(error => console.log(error))
+NProgress.done()
